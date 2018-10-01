d2c6894bafc7dc4773be8b665f44efc5c09f7d24
Logan Williams <loganwilliams116@gmail.com>
2018-09-30 20:26:08 -0700

    Add active user count

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var numActiveUsers = 0;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  numActiveUsers += 1;
+  io.emit('user count', numActiveUsers);
+  
   socket.on('chat message', function(msg){
     socket.broadcast.emit('chat message', msg);
   });
 
-  // console.log('a user connected');
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
+  socket.on('get user count', function(fn) {
+    fn(numActiveUsers);
+  })
+
+  socket.on('disconnect', function(){
+    numActiveUsers -= 1;
+    io.emit('user count', numActiveUsers);
+  });
 });
 
 http.listen(port, function(){
